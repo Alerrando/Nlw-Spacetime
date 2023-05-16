884eee1179bb746eed97adab4ab59135d82b63cc
Alerrando Breno <alerrando2@gmail.com>
2023-05-16 15:30:51 -0300

    structure server finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,16 @@
-import fastify from 'fastify';
+import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
 
 const app = fastify();
+const prisma = new PrismaClient();
+
+app.get("/users", async () => {
+	const users = await prisma.user.findMany();
+	return users;
+});
+
+app.listen({
+	port: 3333,
+}).then(() => {
+	console.log("Server running on http://localhost:3333");
+});
